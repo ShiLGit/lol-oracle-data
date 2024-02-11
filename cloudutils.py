07eef59f75753ffa5454a3bfcf97bdc38c4aa19d
fuc off <lilianshi00@gmail.com>
2024-02-10 20:41:40 -0500

    finish basic cloud util fns

--- cloudutils.py
+++ cloudutils.py
@@ -3,7 +3,16 @@ from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseDownload
 from googleapiclient.http import MediaFileUpload
 import io
+import os
 from googleapiclient.errors import HttpError
+
+# Util fn
+def dict_append(map, key, val):
+    keys = map.keys()
+    if key not in keys: 
+        map[key] = [val]
+    else: 
+        map[key].append(val)
 
 #Create connection
 scope = ['https://www.googleapis.com/auth/drive']
@@ -13,36 +22,45 @@ credentials = service_account.Credentials.from_service_account_file(
                               scopes=scope)
 service = build('drive', 'v3', credentials=credentials)
 
-
-
-# Call the Drive v3 API
+# Get all files on the drive 
 results = service.files().list(pageSize=1000, fields="nextPageToken, files(id, name, mimeType, size, modifiedTime)").execute()
 items = results.get('files', [])
+name_map = dict() 
 
-print(items)
-fid = '1zZfgZCxMPYO7oschzzBuIU2QHQEYs5O0'
-
-## manual upload csv
-# file_metadata = {'name': 'GOLD_III.csv'}
-# media = MediaFileUpload('./GOLD_III.csv',
-#                         mimetype='text/csv')
+#Generate map {'filename': [list of ids under that file name]}
+for r in results['files']:
+    dict_append(name_map, r['name'], r['id'])
+print(name_map)
 
 
-# file = service.files().create(body=file_metadata, media_body=media,
-#                               fields='id').execute()
 
-try: 
-    request_file = service.files().get_media(fileId=fid)
-    file = io.BytesIO()
-    downloader = MediaIoBaseDownload(file, request_file)
-    done = False
-    while done is False:
-        status, done = downloader.next_chunk()
-        print(F'Download {int(status.progress() * 100)}.')
+# Upload file at fpath to google drive api as dst_fname 
+# Return 1 on failure, 0 on success
+def upload(dst_fname, fpath):
+    file_metadata = {'name': f'{dst_fname}'}
 
-    file_retrieved: str = file.getvalue()
-    with open(f"downloaded_file.csv", 'wb') as f:
-        f.write(file_retrieved)
-except HttpError as error:
-    print(F'An error occurred: {error}')
+    try: 
+        media = MediaFileUpload(f'{fpath}')
+        file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+        dict_append(name_map, dst_fname, file['id'])
+        print(f'Successfully uploaded file {file}')
+        return 0
+    except Exception as e: 
+        print(f'Error occurred when trying to upload {fpath} under {dst_fname}: \n\t{e}')
+        return 1
 
+# Delete all files by given fname
+# Return 1 on failure, 0 on success
+def delete_by_fname(fname):
+    deleted = []
+    try: 
+        for id in name_map[fname]:
+            service.files().delete(fileId=id).execute()
+            deleted.append(id)
+            print(f'Successfully delete file of id = {id}')
+        #delete ids from name_map
+        del name_map[fname]
+        return 0
+    except Exception as e:
+            print(e)
+            return 1
